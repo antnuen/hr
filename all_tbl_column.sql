e68117c827a953481841845bef2193f20dac91f4
An Nguyen <92083024+antnuen@users.noreply.github.com>
2023-05-03 15:21:24 +0100

    add files

--- all_tbl_column.sql
+++ all_tbl_column.sql
@@ -1,37 +1,3 @@
 select owner, TABLE_NAME, Column_name, Data_type, data_length, data_precision, NULLABLE, character_Set_Name
 from all_tab_cols 
 where owner = 'HRS'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
